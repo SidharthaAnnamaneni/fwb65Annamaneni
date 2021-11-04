d7d670964df64fd3b19919e8459a0580e30d1a27
Sidhartha Annamaneni <S545265@nwmissouri.edu>
2021-11-03 19:37:14 -0500

    added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kitesRouter = require('./routes/kites');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kites', kitesRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selectormods', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
